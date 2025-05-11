2849293b3b537fd2f676748f469acd6cea1a43a2
Simon Rampp <simon@rampp.dev>
2025-05-11 20:04:31 +0200

    add report

---
+++ ablate/report.py
@@ -0,0 +1,36 @@
+from typing import List
+
+from typing_extensions import Self
+
+from ablate.blocks import AbstractBlock
+from ablate.core.types import Run
+
+
+class Report:
+    def __init__(self, runs: List[Run]) -> None:
+        """Report mapping a list of runs to a list of blocks.
+
+        Args:
+            runs: List of runs to be associated with the report.
+        """
+        self.runs = runs
+        self.blocks: List[AbstractBlock] = []
+
+    def add(self, *blocks: AbstractBlock) -> Self:
+        """Add one or more blocks to the report.
+
+        Returns:
+            The updated report with the added blocks.
+        """
+        for block in blocks:
+            self.blocks.append(block)
+        return self
+
+    def __iadd__(self, block: AbstractBlock) -> Self:
+        self.blocks.append(block)
+        return self
+
+    def __add__(self, block: AbstractBlock) -> Self:
+        r = Report(self.runs)
+        r.blocks = self.blocks + [block]
+        return r
