c615065c1236b385cccb56f18763a556e767966f
Simon Rampp <simon@rampp.dev>
2025-06-04 18:28:55 +0200

    add mock source for testing

--- ablate/sources/__init__.py
+++ ablate/sources/__init__.py
@@ -1,5 +1,18 @@
 from .abstract_source import AbstractSource
+from .autrainer_source import Autrainer
+from .clearml_source import ClearML
 from .mlflow_source import MLflow
+from .mock_source import Mock
+from .tensorboard_source import TensorBoard
+from .wandb_source import WandB
 
 
-__all__ = ["AbstractSource", "MLflow"]
+__all__ = [
+    "AbstractSource",
+    "Autrainer",
+    "ClearML",
+    "MLflow",
+    "Mock",
+    "TensorBoard",
+    "WandB",
+]
