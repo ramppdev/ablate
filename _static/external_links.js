fb62b1c821a90331f0310bf3b395030352a62c7d
ramppdev <ramppdev@users.noreply.github.com>
2025-09-19 14:03:55 +0000

    deploy: 7a65bf19a0ff3f0a92ab5986656a39b173a1fe58

--- _static/external_links.js
+++ _static/external_links.js
@@ -1,3 +1,10 @@
-$(document).ready(function () {
-  $("a.external").attr("target", "_blank").attr("rel", "noopener noreferrer");
+$(function () {
+  $("a.external").each(function () {
+    const u = new URL(this.href, location.origin);
+    if (
+      u.hostname !== "ramppdev.github.io" ||
+      !u.pathname.startsWith("/ablate")
+    )
+      $(this).attr({ target: "_blank", rel: "noopener noreferrer" });
+  });
 });
